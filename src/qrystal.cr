dae7e4e171cafcef3390450af2a6d53150ff8e77
Hadi Hidayat Hammurabi <didikipb007@gmail.com>
2018-12-07 00:39:33 +0700

    Create select and where

--- src/qrystal.cr
+++ src/qrystal.cr
@@ -1,6 +1,36 @@
-# TODO: Write documentation for `Qrystal`
-module Qrystal
-  VERSION = "0.1.0"
+class Qrystal
+  @q = ""
 
-  # TODO: Put your code here
+  def select(table)
+    @q = "SELECT * FROM #{table}"
+    self
+  end
+
+  def where(condition)
+    cond = ""
+    condition.each_with_index do |key, val, index|
+      if index > 0
+        if val.class == String
+          cond = "#{cond} AND #{key}=\"#{val}\""
+        else
+          cond = "#{cond} AND #{key}=#{val}"
+        end
+      elsif
+        if val.class == String
+          cond = "#{cond} #{key}=\"#{val}\""
+        else
+          cond = "#{cond} #{key}=#{val}"
+        end
+      end
+    end
+    @q = "#{@q} WHERE#{cond}"
+    self
+  end
+
+  def string
+    @q
+  end
 end
+
+q = Qrystal.new
+puts q.select("users").where({ nama: "robi", kelas: 2 }).string()
